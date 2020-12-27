2d0e097477d7457dad445690637e4d5570a4451e
Rajeshsingadi <rajeshkumarraje9@gmail.com>
2020-12-28 00:57:25 +0530

    XLarge

---
+++ LetUsCrackCodes/XLarge.py
@@ -0,0 +1,39 @@
+num=input().strip()
+k=int(input())
+l=[]
+st=list(sorted(set(num)))
+for i in range(k):
+    l.append(st[::-1][i])
+print(*l[::-1],sep="")
+
+'''
+
+Number – X Largest Digits Ascending: Accept a number N and print the first X largest digits in N in ascending order.
+
+Input Format:
+The first line contains N and X separated by a space.
+
+
+Output Format:
+The first line contains the X largest digits in N in ascending order.
+
+Boundary Conditions:
+1 <= N <= 999999999999999
+1 <= X <= 10
+
+Example Input/Output 1:
+Input:
+1684285215
+3
+
+Output:
+568
+
+Example Input/Output 2:
+Input:
+90217623
+2
+
+Output:
+79
+'''

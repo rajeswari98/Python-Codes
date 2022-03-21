50417def19c6de5dc01a2f9e93b1f24545f6b783
Rajesh <rajeshkumarraje9@gmail.com>
2022-03-21 12:45:34 +0530

    Extra Method to 16

--- KPOf/PresidentCircleQ16Q31.py
+++ KPOf/PresidentCircleQ16Q31.py
@@ -2,15 +2,31 @@
 #Method 1
 #Time Complexity is O(n)
 
-def classPresident(num, k):
-    if num == 1:
-        return 1
-    else: 
-        return (classPresident(num-1, k)+ k-1) % num+ 1
+# def classPresident(num, k):
+#     if num == 1:
+#         return 1
+#     else: 
+#         return ( classPresident(num-1, k)+ k-1 ) % num+ 1
+# arr= [1,2,3,4,5,6,7]
+# k=5
+# l = len(arr)
+
+# IndexValue = classPresident(l, k)
+# print(arr[IndexValue-1])
+
+
+#Method 2
+def classPresident(arr, k):
+    k = k-1
+    indx = k
+    while len(arr) > 1:
+        arr.pop(indx)
+        indx = (indx + k) % len(arr)
+    return arr[indx]
+
+
 arr= [1,2,3,4]
-k=2
-l = len(arr)
+k = 2
+res = classPresident(arr, k)
+print(res)
 
-
-IndexValue = classPresident(l, k)
-print(arr[IndexValue-1])
